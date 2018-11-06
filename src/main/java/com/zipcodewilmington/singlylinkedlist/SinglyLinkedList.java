65eaaf8f3dcdab5dc7483ef765866e466b81fb46
Jacob Andersen <jacobandersen@zipcoders-MacBook-Pro-77.local>
2018-11-06 09:09:51 -0500

    basic methods done, working on sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,128 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    private class Node
+    {
+        private String item;
+        private Node link;
+
+        public Node()
+        {
+            link = null;
+            item = null;
+        }
+
+        public Node (String newItem, Node linkValue)
+        {
+            item = newItem;
+            link = linkValue;
+        }
+    }
+
+    private Node head;
+
+    public SinglyLinkedList()
+    {
+        head = null;
+    }
+
+    public void addToStart(String itemName)
+    {
+        head = new Node(itemName, head);
+    }
+
+    public int size()
+    {
+        int count =0;
+        Node position = head;
+        while (position != null)
+        {
+            count++;
+            position = position.link;
+        }
+        return count;
+    }
+
+    public boolean contains(String item)
+    {
+        return(find(item) != null);
+
+    }
+
+    private Node find(String target)
+    {
+        Node position = head;
+        String itemAtPosition;
+        while (position != null)
+        {
+        itemAtPosition = position.item;
+        if (itemAtPosition.equals(target))
+            return position;
+        position = position.link;
+        }
+        return null;
+        }
+
+        public String get(int index)
+        {
+            Node position = head;
+            String itemAtPosition;
+            int i=0;
+            while (position != null)
+            {
+                i++;
+                itemAtPosition = position.item;
+            if (index == i)
+                return itemAtPosition;
+            position=position.link;
+            }
+            return null;
+        }
+
+        public void remove (int index) {
+            Node position = head;
+            if (index == 0) {
+                head = head.link;
+            }
+            int i = 0;
+            while (position != null && i < index - 1) {
+                i++;
+                position = position.link;
+            }
+            if (position == null || position.link == null)
+                return;
+
+            position.link = position.link.link;
+        }
+
+        public int findIndex(String target){
+
+            Node position = head;
+            String itemAtPosition;
+            int i=0;
+            while (position != null)
+            {
+                i++;
+                itemAtPosition = position.item;
+                if (itemAtPosition.equals(target))
+                    return i;
+                position = position.link;
+            }
+            return -1;
+
+        }
+
+        public SinglyLinkedList copyList()
+        {
+            Node position = head;
+            Node copynode = new Node();
+            SinglyLinkedList copy = new SinglyLinkedList();
+            while (position!=null) {
+                copynode.item = position.item;
+                copynode.link = position.link;
+                position=position.link;
+            }
+            return copy;
+        }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,96 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+  @Test
+    public void addTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apples");
+        list.addToStart("oranges");
+        list.addToStart("bananas");
+        int expected = 3;
+        int actual = list.size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void sizeTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apples");
+        int expected = 1;
+        int actual = list.size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void getTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apples");
+        String expected = "Apples";
+        String actual = list.get(1);
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void containsTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apples");
+        boolean expected = true;
+        boolean actual = list.contains("Apples");
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void findIndexTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apples");
+        int expected = 1;
+        int actual = list.findIndex("Apples");
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void copyTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apple");
+        list.addToStart("Microsoft");
+        list.addToStart("Google");
+        SinglyLinkedList copylist = list.copyList();
+        Assert.assertTrue(!list.equals(copylist));
+    }
+
+    @Test
+    public void removeTest()
+    {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addToStart("Apple");
+        list.addToStart("Microsoft");
+        list.addToStart("Google");
+        list.remove(2);
+        int expected=2;
+        int actual = list.size();
+        Assert.assertEquals(expected,actual);
+    }
+
+
 }
